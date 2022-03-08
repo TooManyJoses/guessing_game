8fb036f106fe4762904eb70b6bf7130e15ec1a97
Jose-Antonio D. Rubio <jadrubio@gmail.com>
2022-03-08 17:44:45 -0500

    Basic set up to prompt user for a number, read it in, display it back

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the guess the number game!");
+
+    println!("Please enter your guess");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("An error has occured.");
+
+    println!("You guessed: {}", guess);
 }
