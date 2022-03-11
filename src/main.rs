01c2457a0dc61eccd80f02c68a33f9641e4f142d
Jose-Antonio D. Rubio <jadrubio@gmail.com>
2022-03-11 16:39:20 -0500

    shadowing of guess input to convert to u32 and added in match to compare

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Welcome to the guess the number game!");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is {}", secret_number);
 
     println!("Please enter your guess");
 
@@ -9,5 +15,14 @@ fn main() {
 
     io::stdin().read_line(&mut guess).expect("An error has occured.");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too low"),
+        Ordering::Equal => println!("Correct"),
+        Ordering::Greater => println!("Too high"),
+    }
 }
